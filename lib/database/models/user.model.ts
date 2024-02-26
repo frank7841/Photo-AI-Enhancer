d0f85312a82cc0d6d29fa60a8de3bf76ef9d36ae
frankesomcare <frankline.wakasiaka@esomcare.com>
2024-02-26 17:16:52 +0300

    models -3

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,26 @@
+
+import { Schema, model, models } from "mongoose";
+// clerkid email username firstName photo lastName planId creditballance
+
+const UserSchema = new Schema({
+    clerkId:{type:String,
+            required:true,
+            unique:true},
+
+    email:{type:String, 
+            required:true,
+            unique:true},
+    userName:{type:String,
+         unique:true},
+    photo:{type:String},
+
+    firstName:{type:String},
+    lastName:{type:String},
+    planId:{type:Number, 
+            default:1},
+    creditBalance:{type:Number,
+                   default:10 },
+    
+});
+const User = models?.User|| model('User', UserSchema);
+export default User;
